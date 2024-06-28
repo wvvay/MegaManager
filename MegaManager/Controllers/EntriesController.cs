29e3cb860de6cac1045be143bab3c2bdde2a450a
phenya <ramil_islamov_01@mail.ru>
2024-06-28 21:44:46 +0300

    Копирование и удаление элементов.

--- MegaManager/Controllers/EntriesController.cs
+++ MegaManager/Controllers/EntriesController.cs
@@ -1,4 +1,5 @@
 ﻿using MegaManager.Models;
+using MegaManager.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,38 +8,44 @@ namespace MegaManager.Controllers
     [Authorize]
     public class EntriesController : Controller
     {
+        private ISession _session => HttpContext.Session;
+        private const string EntriesKey = "Entries";
+
         public IActionResult Index()
         {
-            //Обратиться к БД и получить список обьектов класса Entry
-
-            var entries = new List<Entry>
+            // Получаем данные Entries из сессии или создаем новый список
+            var entries = _session.GetObject<List<Entry>>(EntriesKey) ?? new List<Entry>
             {
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example1@example.com", Password = "password1" },
-                new Entry { Address = "example2@example.com", Password = "password2" },
-                new Entry { Address = "example3@example.com", Password = "password3" }
+                new Entry { Id = 1, Address = "example1@example.com", Password = "password1" },
+                new Entry { Id = 2, Address = "example2@example.com", Password = "password2" },
+                new Entry { Id = 3, Address = "example3@example.com", Password = "password3" }
             };
-
+            _session.SetObject(EntriesKey, entries);
 
             return View(entries);
+        }
+
+        // GET: Entries/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            
+
+            // Получаем данные Entries из сессии
+            var entries = _session.GetObject<List<Entry>>(EntriesKey) ?? new List<Entry>();
+
+            var entryToRemove = entries.FirstOrDefault(e => e.Id == id);
+            if (entryToRemove == null)
+                return NotFound();
+            
+
+            entries.Remove(entryToRemove);
+
+            // Сохраняем обновленный список Entries в сессию
+            _session.SetObject(EntriesKey, entries);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- MegaManager/Models/Entry.cs
+++ MegaManager/Models/Entry.cs
@@ -2,6 +2,7 @@
 {
     public class Entry
     {
+        public int Id { get; set; }
         public string Address { get; set; }
         public string Password { get; set; }
     }
