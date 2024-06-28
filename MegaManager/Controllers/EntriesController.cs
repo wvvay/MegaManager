fc615d605594ef1967ebbdf2526dffe1f0219b59
phenya <ramil_islamov_01@mail.ru>
2024-06-28 20:11:55 +0300

    Отображение данных

--- MegaManager/Controllers/EntriesController.cs
+++ MegaManager/Controllers/EntriesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MegaManager.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MegaManager.Controllers
@@ -6,6 +7,38 @@ namespace MegaManager.Controllers
     [Authorize]
     public class EntriesController : Controller
     {
-        public IActionResult Index() => View();
+        public IActionResult Index()
+        {
+            //Обратиться к БД и получить список обьектов класса Entry
+
+            var entries = new List<Entry>
+            {
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example1@example.com", Password = "password1" },
+                new Entry { Address = "example2@example.com", Password = "password2" },
+                new Entry { Address = "example3@example.com", Password = "password3" }
+            };
+
+
+            return View(entries);
+        }
     }
 }
